287037df24c591112f1e52af6a57cbd5af75340a
John Mbayu <jmbayu@yahoo.de>
2021-06-08 22:27:34 +0200

    refactoring

--- centos/do.sh
+++ centos/do.sh
@@ -3,7 +3,9 @@ ansible-playbook playbooks/cleanup_cni_node.yml
 ansible-playbook setup_master_node.yml
 ansible-playbook setup_worker_nodes.yml
 
-kubectl get nodes
+ssh 10.1.1.171 sudo cat /root/.kube/config>~/.kube/config
+
+kubectl get nodes -o wide
 
 # metallb 
 helm install metallb bitnami/metallb --namespace kube-system --set configInline.address-pools[0].name=default --set configInline.address-pools[0].protocol=layer2 --set configInline.address-pools[0].addresses[0]=10.1.1.201-10.1.1.254
@@ -51,6 +53,9 @@ spec:
       persistentVolumeClaim:
         claimName: test-claim
 EOF
+
+kubectl get all --all-namespaces -o wide
+
 exit 0
 
 
